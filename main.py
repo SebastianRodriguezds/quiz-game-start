2d03e55579062eb8ff220ccd59c06fbe9e9ff3eb
Sebastian Rodriguez <sebastianrodriguezds@gmail.com>
2024-11-13 14:21:22 +0000

    Class QuizBrain Added

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
 import question_model
 import data
 import random
-
-questions_list = []
-
-def make_question(text, answer):
-  question = question_model.Question(text, answer)
-  questions_list.append(question)
-
-for item in data.question_data:
-  make_question(item["text"] , item["answer"])
+from quiz_brain import QuizBrain
+def make_question():
+  for item in data.question_data:
+    question = question_model.Question(item["text"] , item["answer"])
+    questions_list.append(question)
 
 def return_question():
   question = random.choice(questions_list)
   return question
-
-
 
 def check_answer(guess, answer):
   if answer == guess:
@@ -23,21 +17,10 @@ def check_answer(guess, answer):
   return False
 
 
-
-game_is_playing = True
-
-
-while game_is_playing:
-  question = return_question()
-  print(question.text)
-  print(question.answer)
-  user_answer = input("is either 'True' or 'False'")
-  if check_answer(user_answer, question.answer):
-    print("Repsuesta correcta")
-  else:
-    print("Incorrecta")
-  
-  game_is_playing = False
+questions_list = []
+make_question()
+quiz = QuizBrain(questions_list)
+quiz.next_question()
 
 
 
